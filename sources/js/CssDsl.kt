40d32bc4f2a0f561432eebc2513131f7b6a0ecc1
Marc Knaup <marc@knaup.io>
2020-11-28 22:58:52 +0000

    .

--- sources/js/CssDsl.kt
+++ sources/js/CssDsl.kt
@@ -2,4 +2,6 @@ package io.fluidsonic.css
 
 
 @DslMarker
+@Retention(AnnotationRetention.BINARY)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY, AnnotationTarget.TYPEALIAS)
 public annotation class CssDsl
