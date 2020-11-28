b3d5d59d1d7b327e144ca10c032be4b2902f130a
Marc Knaup <marc@knaup.io>
2020-11-28 20:14:21 +0000

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,17 @@
 import io.fluidsonic.gradle.*
+import org.jetbrains.kotlin.gradle.plugin.*
 
 plugins {
-	id("io.fluidsonic.gradle") version "1.1.12"
+	id("io.fluidsonic.gradle") version "1.1.13"
 }
 
 fluidLibrary(name = "css", version = "0.9.0")
 
 fluidLibraryModule(description = "FIXME") {
+	publishSingleTargetAsModule()
+
 	targets {
-		js {
+		js(KotlinJsCompilerType.IR) {
 			dependencies {
 				implementation(npm("@emotion/stylis", "~0.8.5"))
 			}
