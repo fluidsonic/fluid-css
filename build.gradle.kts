745c2934e0a5ee31e44d498fc7e478185570b7ef
Marc Knaup <marc@knaup.io>
2023-06-24 16:41:49 +0700

    🎉 0.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.3.2"
 }
 
-fluidLibrary(name = "css", version = "0.10.0")
+fluidLibrary(name = "css", version = "0.11.0")
 
 fluidLibraryModule(description = "FIXME") {
 	targets {
